cd267d83b892bd51b5507a4915925d9f1318b728
ekansrm <ekansrm@gmail.com>
2017-12-14 09:00:43 +0800

    使用预训练的Word2Vec

--- LSTM/lstmTest.py
+++ LSTM/lstmTest.py
@@ -36,6 +36,10 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM, GRU
+from keras.optimizers import SGD
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
 
 
 neg = pd.read_excel('../data/taobao-comment/neg.xls', header=None, index=None)
@@ -70,16 +74,43 @@ model.add(Embedding(name='embedding',
                     weights=[weights],
                     trainable=False))
 
-model.add(LSTM(256))  # try using a GRU instead, for fun
-model.add(Dropout(0.5))
+model.add(LSTM(512))  # try using a GRU instead, for fun
+model.add(Dropout(0.9))
+model.add(Dense(32))
+model.add(Dropout(0.9))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam')
+MODEL_LSTM_PATH = 'sentiment-analysis-lstm'
+model_checkpoint_better_path = MODEL_LSTM_PATH + '.checkpoint' \
+                                                 '.epoch-{epoch:02d}' \
+                                                 '.val_loss-{val_loss:.6f}' \
+                                                 '.val_acc-{val_acc:.6f}'
+model_checkpoint_best_path = MODEL_LSTM_PATH + '.checkpoint.best'
 
-model.fit(xa, ya, batch_size=32, nb_epoch=100, validation_split=0.2)  # 训练时间为若干个小时
+sgd = SGD(lr=0.0, momentum=0.9, decay=0.0, nesterov=False)
 
-model.save("sentiment-analysis-lstm-keras.hdf5")
+# def step_decay(epoch):
+#     initial_lrate = 0.1
+#     drop = 0.5
+#     epochs_drop = 10.0
+#     lrate = initial_lrate * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
+#     return lrate
+# lrate = LearningRateScheduler(step_decay)
+
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.0001, mode='min', patience=5, verbose=1)
+
+checkpoint_better = ModelCheckpoint(
+    model_checkpoint_better_path, save_best_only=True, monitor='val_acc',  mode='max', verbose=1)
+
+checkpoint_best = ModelCheckpoint(
+    model_checkpoint_best_path, save_best_only=True, monitor='val_acc',  mode='max', verbose=1)
+
+model.compile(loss='binary_crossentropy', optimizer='adam',  metrics=['accuracy'])
+
+model.fit(xa, ya, batch_size=32, nb_epoch=100, validation_split=0.2, shuffle=True,
+          callbacks=[checkpoint_better, checkpoint_best, early_stopping]
+          )  # 训练时间为若干个小时
 
 e = model.evaluate(xt, yt)
 print('Test accuracy:', e)
