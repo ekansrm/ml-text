9f3359f6cc0d56ad9cdad805e07a8eeb23f37c7b
kami.wm <kami.wm@alibaba-inc.com>
2018-01-29 21:08:16 +0800

    ngrams

--- move_this/ngram.py
+++ move_this/ngram.py
@@ -49,7 +49,7 @@ def _estimator(fdist, *estimator_args, **estimator_kwargs):
 
 class BaseNgramCounter(object):
     def __init__(self, order, super_ngrams, train, pad_left=True, pad_right=False,
-                 estimator=None, *estimator_args, **estimator_kwargs):
+                 estimator=None, left_pad_symbol=None, right_pad_symbol=None, *estimator_args, **estimator_kwargs):
 
         if super_ngrams is not None:
             self.ngrams = super_ngrams
@@ -178,49 +178,54 @@ class NgramsModel(BaseNgramModel):
     def __init__(self, n, train, pad_left=True, pad_right=False,
                  estimator=None, *estimator_args, **estimator_kwargs):
 
-        # protection from cryptic behavior for calling programs
-        # that use the pre-2.0.2 interface
+        # 检查参数类型
         assert (isinstance(pad_left, bool))
         assert (isinstance(pad_right, bool))
 
-        # make sure n is greater than zero, otherwise print it
+        # 确保阶数大于零
         assert (n > 0), n
 
-        # For explicitness save the check whether this is a unigram model
+        # 判断是否为unigram
         self.is_unigram_model = (n == 1)
-        # save the ngram order number
+        # 保存模型阶数
         self._n = n
-        # save left and right padding
+
+        # 保存padding, padding长度最多为 n-1
         self._lpad = ('',) * (n - 1) if pad_left else ()
         self._rpad = ('',) * (n - 1) if pad_right else ()
 
         if estimator is None:
-            estimator = _estimator
+            estimator = LidstoneProbDist
 
-        # TODO ConditionalFreqDist 不再从 Freqdict 继承, 没有 freqdict 方法和 B 方法, 所以另外记录 freqDict
-        cfd = ConditionalFreqDist()
-        freqdict = FreqDist()
-
-        # set read-only ngrams set (see property declaration below to reconfigure)
-        self._ngrams = set()
-
-        # If given a list of strings instead of a list of lists, create enclosing list
+        # train期待类型: List<List<String>>
         if (train is not None) and isinstance(train[0], compat.string_types):
             train = [train]
 
+        # TODO ConditionalFreqDist 不再从 Freqdict 继承, 没有 freqdict 方法和 B 方法, 所以另外记录 freqDict
+
+        # cfd 用来计算方法 c(w_i|w_(i-1)w_(i-2)...w(i-(n-1)))
+        self.c = ConditionalFreqDist()
+        freqdict = FreqDist()
+
+        # ngrams的tuple的集合
+        self._ngrams = set()
+
         for sent in train:
+            # ['a', 'b', 'c'] -> [('a', 'b'), ('b', 'c')]
             raw_ngrams = ngrams(sent, n, pad_left, pad_right, left_pad_symbol=' ', right_pad_symbol=' ')
             for ngram in raw_ngrams:
+                ngram = tuple(ngram)
                 self._ngrams.add(ngram)
-                context = tuple(ngram[:-1])
+                context = ngram[:-1]
                 token = ngram[-1]
-                # TODO 这里修改过
-                cfd[context][token] += 1
+                self.c[context][token] += 1
+
                 freqdict[ngram] += 1
 
-        self._probdist = estimator(freqdict, *estimator_args, **estimator_kwargs)
+        # 通过概率估计器得到关于 ngrams 的概率字典
+        self._prob_dist = estimator(freqdict, *estimator_args, **estimator_kwargs)
 
-        # recursively construct the lower-order models
+        # 如果不是 unigram, 我们需要生产低阶 ngrams 模型
         if not self.is_unigram_model:
             self._backoff = NgramsModel(n - 1, train,
                                         pad_left, pad_right,
@@ -229,8 +234,9 @@ class NgramsModel(BaseNgramModel):
                                         **estimator_kwargs)
 
             self._backoff_alphas = dict()
-            # For each condition (or context)
-            for ctxt in cfd.conditions():
+
+            # 对于每一个条件(w_(i-1), w_(i-2), ..., w(i-(n-1))
+            for ctxt in self.c.conditions():
                 backoff_ctxt = ctxt[1:]
                 backoff_total_pr = 0.0
                 total_observed_pr = 0.0
@@ -238,7 +244,7 @@ class NgramsModel(BaseNgramModel):
                 # this is the subset of words that we OBSERVED following
                 # this context.
                 # i.e. Count(word | context) > 0
-                for word in self._words_following(ctxt, cfd):
+                for word in self._words_following(ctxt):
                     _pr = self.prob(word, ctxt)
                     total_observed_pr += _pr
                     # we also need the total (n-1)-gram probability of
@@ -259,21 +265,28 @@ class NgramsModel(BaseNgramModel):
 
                 self._backoff_alphas[ctxt] = alpha_ctxt
 
-    def _words_following(self, context, cond_freq_dist):
+    def _words_following(self, context):
 
         # TODO 需要做兼容处理 Py2 Py3
-        # for ctxt, word in cond_freq_dist.items():
-        #     if ctxt == context:
-        #         yield word.values()
-        if context in cond_freq_dist:
-            return cond_freq_dist[context]
+        if context in self.c:
+            return self.c[context].keys()
+
+    def _alpha(self, context):
+        """Get the backoff alpha value for the given context
+        """
+        assert not self.is_unigram_model, "Alphas and backoff are not defined for unigram models"
+
+        if context in self._backoff_alphas:
+            return self._backoff_alphas[context]
+        else:
+            return 1
 
     def score(self, word, context):
         return self.prob(word=word, context=context)
 
     def prob(self, word, context):
         """
-        Evaluate the probability of this word in this context using Katz Backoff.
+        通过 katz Backoff 模型估计输入的词在输入的上下文中的概率
 
         :param word: the word to get the probability of
         :type word: str
@@ -284,21 +297,10 @@ class NgramsModel(BaseNgramModel):
         _ngrams = context + (word,)
         if _ngrams in self._ngrams or self.is_unigram_model:
             # TODO 这里有修改
-            _prob = self._probdist.prob(_ngrams)
+            _prob = self._prob_dist.prob(_ngrams)
             return _prob
         else:
             return self._alpha(context) * self._backoff.prob(word, context[1:])
-
-    def _alpha(self, context):
-        """Get the backoff alpha value for the given context
-        """
-        error_message = "Alphas and backoff are not defined for unigram models"
-        assert not self.is_unigram_model, error_message
-
-        if context in self._backoff_alphas:
-            return self._backoff_alphas[context]
-        else:
-            return 1
 
     def logprob(self, word, context):
         """
